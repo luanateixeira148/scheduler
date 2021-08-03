097363bc52d23b7fbb1950029a9c2e46fc4e2cb8
Luana <luanateixeira148@gmail.com>
2021-08-03 14:19:33 -0700

    Update app with DayList component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -6,7 +6,7 @@ const DayList = function(props) {
     return (
       <ul>
         <DayListItem 
-          key={day.if}
+          key={day.id}
           name={day.name} 
           spots={day.spots} 
           selected={day.name === props.day}
