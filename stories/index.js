01aec79917df6bdcf5c3da80a7ed523e3e722d0a
Luana <luanateixeira148@gmail.com>
2021-08-04 14:54:13 -0700

    create Empty component

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 /***********/
 
@@ -153,4 +154,4 @@ storiesOf("Appointment", module)
     />
   )
   .add("Header", () => <Header time="12pm" />)
-
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
