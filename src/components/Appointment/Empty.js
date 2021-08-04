f2aed00beff132e401607aa8bd72704708c9fe4a
Luana <luanateixeira148@gmail.com>
2021-08-04 15:33:53 -0700

    create Status component

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 const Empty = function (props) {
-
   return (
-
     <main className="appointment__add">
       <img
         className="appointment__add-button"
@@ -12,9 +10,7 @@ const Empty = function (props) {
         onClick={props.onAdd}
       />
     </main>
-
-  )
-
-}
+  );
+};
 
 export default Empty;
