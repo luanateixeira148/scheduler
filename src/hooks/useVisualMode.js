97034a4f43be77387784788b5b266c23959dc400
Luana <luanateixeira148@gmail.com>
2021-08-09 10:42:24 -0700

    add comments to useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,9 +4,11 @@ const useVisualMode = function (initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([]);
 
+  // take in a new mode and update the mode state with the new value.
   const transition = function (newMode, replace = false) {
     const newHist = [...history];
-
+    
+    //When replace is true then set the history to reflect that we are replacing the current mode.
     if (replace) {
       newHist.push(newMode);
     }
@@ -16,20 +18,24 @@ const useVisualMode = function (initial) {
       newHist.push(newMode);
     }
 
+    // add the new mode to our history
     setMode(newMode);
     setHistory(newHist);
   };
 
+  // allow us to return to the previous mode
   const back = function () {
-    if (history.length <= 0) {
+    if (history.length < 1) {
       return;
     }
 
-    if (history.length > 0) {
+    if (history.length >= 1) {
       const newHist = [...history];
       newHist.pop();
 
+      // set the mode to the previous item in our history array
       setMode(newHist[newHist.length - 2]);
+
       setHistory(newHist);
     }
   };
