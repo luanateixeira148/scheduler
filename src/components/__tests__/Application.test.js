8a434b06ed5a6e36424143c2c450285aa842f9c9
Luana <luanateixeira148@gmail.com>
2021-08-11 18:20:13 -0700

    add mocking modules

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,24 +1,22 @@
-/*
-  We are rendering `<Application />` down below, so we need React.createElement
-*/
 import React from "react";
 
-/*
-  We import our helper functions from the react-testing-library
-  The render function allows us to render Components
-*/
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
-/*
-  We import the component that we are testing
-*/
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-/*
-  A test that renders a React Component
-*/
-it("renders without crashing", () => {
-  render(<Application />);
+describe("Application", () => {
+
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+  
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
+  });
+
 });
+
+// npm test -- --coverage --watchAll=false --> command to generate the coverage report.
