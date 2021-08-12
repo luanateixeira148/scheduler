fe13c79f20eba3f3c2bbe81629375ec513fb166d
Luana <luanateixeira148@gmail.com>
2021-08-12 14:08:19 -0700

    clean up components tests

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,9 +1,8 @@
-
 import React from "react";
 
 import { render, cleanup } from "@testing-library/react";
 
-import Appointment from "components/Appointment"
+import Appointment from "components/Appointment";
 
 describe("Appointment", () => {
   it("renders without crashing", () => {
