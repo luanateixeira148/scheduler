4c88687d61c5536668fbe26ea67664b76b62bb5c
Luana <luanateixeira148@gmail.com>
2021-08-12 14:04:13 -0700

    cleanup Cypress code

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -5,12 +5,9 @@ describe("Navigation", () => {
 
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-  
+
     cy.contains("[data-testid=day]", "Tuesday")
       .click()
-      .should("have.class", "day-list__item--selected")
+      .should("have.class", "day-list__item--selected");
   });
-
 });
-
-// visit: http://localhost:8001/api/debug/reset to reset db after each test
