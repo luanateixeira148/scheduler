5c0d20c79d183d79d376a73f1f95545549c3da6a
Luana <luanateixeira148@gmail.com>
2021-08-12 14:11:20 -0700

    cleanup hooks

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,19 +2,19 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
-  
+
   function transition(mode, replace) {
-    setHistory(prev =>
+    setHistory((prev) =>
       replace ? [...prev.slice(0, prev.length - 1), mode] : [...prev, mode]
     );
   }
-  
+
   function back() {
-    setHistory(prev => {
+    setHistory((prev) => {
       if (prev.length < 2) return prev;
-      return prev.slice(0, prev.length - 1)
+      return prev.slice(0, prev.length - 1);
     });
   }
-  
+
   return { mode: history[history.length - 1], transition, back };
 }
